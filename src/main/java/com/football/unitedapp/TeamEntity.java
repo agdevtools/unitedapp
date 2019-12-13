30fda0e3b9344b46cdc065fa79cacc5bd20c7e62
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2019-12-13 23:45:59 +0000

    refactored and removed redudant getters and setters

--- src/main/java/com/football/unitedapp/TeamEntity.java
+++ src/main/java/com/football/unitedapp/TeamEntity.java
@@ -1,7 +1,6 @@
 package com.football.unitedapp;
 
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
@@ -9,33 +8,10 @@ import org.springframework.data.cassandra.core.mapping.Table;
 
 @Table(value="team")
 public class TeamEntity {
-    private static final String insertQuery = "INSERT into team(" +
-            "playerid, " +
-            "player_name) " +
-            "values(";
-
-
     @PrimaryKeyColumn(value = "playerid", type = PrimaryKeyType.PARTITIONED, name = "playerid")
-    @Id
     public int playerId;
     @Column(value = "player_name")
     public String playerName;
-
-    public int getPlayerId() {
-        return playerId;
-    }
-
-    public void setPlayerId(int playerId) {
-        this.playerId = playerId;
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
 
     public TeamEntity(int playerId, String playerName) {
         this.playerId = playerId;

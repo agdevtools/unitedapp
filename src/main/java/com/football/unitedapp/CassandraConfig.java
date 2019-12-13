12157b51445852c4d7370fd01e18a2cb56b16c5a
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2019-12-13 23:39:22 +0000

    added cassandra config to application properties

--- src/main/java/com/football/unitedapp/CassandraConfig.java
+++ src/main/java/com/football/unitedapp/CassandraConfig.java
@@ -1,5 +1,6 @@
 package com.football.unitedapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
@@ -10,14 +11,13 @@ import org.springframework.data.cassandra.repository.config.EnableCassandraRepos
 @Configuration
 @EnableCassandraRepositories
 public class CassandraConfig extends AbstractCassandraConfiguration {
-    //@Value("${spring.data.cassandra.contactPoints}")
-    private String contactPoints = "54.172.11.243";
-   // @Value("${spring.data.cassandra.port}")
-    private int port = 9042;
-   // @Value("${spring.data.cassandra.keyspace}")
+    @Value("${contactPoints}")
+    private String contactPoints;
+    @Value("${port}")
+    private int port;
+    @Value("${keySpace}")
     private String keySpace = "united";
-//    @Value("${cassandra.basePackages}")
-//    private String basePackages;
+
     @Override
     protected String getKeyspaceName() {
         return keySpace;

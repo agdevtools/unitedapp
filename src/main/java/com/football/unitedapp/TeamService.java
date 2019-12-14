051916a77d9258b0ab8263c30bbc55ab00713623
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2019-12-14 16:32:55 +0000

    refactored

--- src/main/java/com/football/unitedapp/TeamService.java
+++ src/main/java/com/football/unitedapp/TeamService.java
@@ -1,10 +1,7 @@
 package com.football.unitedapp;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
 public interface TeamService {
 
     List<TeamEntity> getTeam();

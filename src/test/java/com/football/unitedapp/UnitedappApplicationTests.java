445e3a5e648153fc5164cd4df30c0d8df75a45d9
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2019-12-20 17:03:38 +0000

    added captor test

--- src/test/java/com/football/unitedapp/UnitedappApplicationTests.java
+++ src/test/java/com/football/unitedapp/UnitedappApplicationTests.java
@@ -1,56 +1,33 @@
 package com.football.unitedapp;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
+import org.mockito.*;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@SpringBootTest
 class UnitedappApplicationTests {
 
+
+	@Mock
+	private TeamRepository teamRepository;
+
+	@InjectMocks
+	private TeamServiceImpl teamService;
+
 	@Captor
-	private ArgumentCaptor<TeamEntity> teamEntityArgumentCaptor = ArgumentCaptor.forClass(TeamEntity.class);
-
-	private TeamServiceImpl teamServiceImpl;
-
-	@BeforeEach
-	public void setUp() {
-		teamServiceImpl = mock(TeamServiceImpl.class);
-	}
+	private ArgumentCaptor<TeamEntity> captor;
 
 	@Test
-	void test_TeamServiceImpl_getTeamByPlayerId_returnsSingleTeamEntity() {
-
-		TeamEntity expectedTeamEntity = new TeamEntity(5,"Harry Maguire");
-
-		when(teamServiceImpl.getPlayer(anyInt())).thenReturn(expectedTeamEntity);
-
-		assertEquals("Harry Maguire", expectedTeamEntity.getPlayerName());
-		assertEquals(5, expectedTeamEntity.getPlayerId());
+	public void shouldCapture() {
+		TeamEntity expectedPlayer = new TeamEntity(11,"Ryan Giggs");
+		teamService.createPlayer(expectedPlayer);
+		Mockito.verify(teamRepository).save(captor.capture());
+		TeamEntity actualPlayer = captor.getValue();
+		Assertions.assertThat(actualPlayer.getPlayerName()).isEqualTo(expectedPlayer.getPlayerName());
+		Assertions.assertThat(actualPlayer.getPlayerId()).isEqualTo(expectedPlayer.getPlayerId());
 	}
 
-	@Test
-	void test_TeamServiceImpl_getTeam_returnsListofTeamEntity() {
 
-		TeamEntity expectedTeamEntity1 = new TeamEntity(5, "Harry Maguire");
-		TeamEntity expectedTeamEntity2 = new TeamEntity(6, "Paul Pogba");
-		List<TeamEntity> expectedTeamEntityList = new ArrayList<TeamEntity>();
-		expectedTeamEntityList.add(expectedTeamEntity1);
-		expectedTeamEntityList.add(expectedTeamEntity2);
 
-		when(teamServiceImpl.getTeam()).thenReturn(expectedTeamEntityList);
-
-		List<TeamEntity> actualTeamEntityList = teamServiceImpl.getTeam();
-		assertEquals(expectedTeamEntityList, actualTeamEntityList);
-	}
-
-	}
+}

d505b541fa46aa086289734e1243ed092c7d7b8c
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2020-01-03 21:04:37 +0000

    added webMvcTagsProvider for metrics tags

--- src/main/java/com/football/unitedapp/AppConfig.java
+++ src/main/java/com/football/unitedapp/AppConfig.java
@@ -1,7 +1,14 @@
 package com.football.unitedapp;
 
+import io.micrometer.core.instrument.ImmutableTag;
+import io.micrometer.core.instrument.Tag;
+import org.springframework.boot.actuate.metrics.web.servlet.WebMvcTagsProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 @Configuration
 public class AppConfig {
@@ -13,6 +20,35 @@ public class AppConfig {
 
     public TeamController teamController(TeamServiceImpl teamServiceImpl) {
         return new TeamController(teamServiceImpl);
+    }
+
+    @Bean
+    public WebMvcTagsProvider webMvcTagsProvider() {
+        return new WebMvcTagsProvider() {
+            @Override
+            public Iterable<Tag> getTags(HttpServletRequest request, HttpServletResponse response, Object handler, Throwable exception) {
+                ArrayList<Tag> tags = new ArrayList<>();
+
+                tags.add(new ImmutableTag("orgId", "monitoring"));
+                tags.add(new ImmutableTag("method", request.getMethod()));
+                tags.add(new ImmutableTag("endpoint", getRequest_mapping(request)));
+                tags.add(new ImmutableTag("status", String.valueOf(response.getStatus())));
+
+                return tags;
+            }
+
+            private String getRequest_mapping(HttpServletRequest request) {
+                String reqMapping = (String) request.getAttribute("bestMatchingPattern");
+                return reqMapping == null ? "no route" : reqMapping;
+            }
+
+            @Override
+            public Iterable<Tag> getLongRequestTags(HttpServletRequest request, Object handler) {
+                return new ArrayList<>();
+            }
+
+        };
+
     }
 
 }

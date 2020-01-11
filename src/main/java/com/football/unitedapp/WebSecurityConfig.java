3dbc9b028ca145cc13369a80679598eb964463be
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2020-01-11 23:36:09 +0000

    added profiles for testing and dev, added WebSecurity config

--- src/main/java/com/football/unitedapp/WebSecurityConfig.java
+++ src/main/java/com/football/unitedapp/WebSecurityConfig.java
@@ -19,6 +19,7 @@ public class WebSecurityConfig
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()

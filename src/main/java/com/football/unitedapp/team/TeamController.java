05c74697b6d06afcca9604ffe298567a0c2bb023
Armand Gaillard <armandgaillard@armands-air.home>
2021-02-21 16:08:57 +0000

    added kafka listener to pickup footiestats api requests and log in t_logs

--- src/main/java/com/football/unitedapp/team/TeamController.java
+++ src/main/java/com/football/unitedapp/team/TeamController.java
@@ -35,21 +35,21 @@ public class TeamController {
     @GetMapping("/team")
     public TeamResponse getTeam()
     {
-        dbLoggingService.logToDatabase(requestDate,"/team","GET","");
+        dbLoggingService.logToDatabase(new Date(System.currentTimeMillis()),"/unitedapp/team","GET","");
         return teamServiceImpl.getTeam();
     }
 
     @PostMapping("/team")
     @ResponseStatus(HttpStatus.CREATED)
     public TeamResponse createPlayer(@RequestBody TeamRequest teamRequest) {
-        dbLoggingService.logToDatabase(requestDate,"/team","POST",formatRequestForLogs(teamRequest));
+        dbLoggingService.logToDatabase(new Date(System.currentTimeMillis()),"/unitedapp/team","POST",formatRequestForLogs(teamRequest));
           return teamServiceImpl.createPlayer(teamRequest);
     }
 
     @PutMapping("/team")
     @ResponseStatus(HttpStatus.OK)
     public TeamResponse updatePlayer(@RequestBody TeamRequest teamRequest) {
-        dbLoggingService.logToDatabase(requestDate,"/team","PUT",formatRequestForLogs(teamRequest));
+        dbLoggingService.logToDatabase(new Date(System.currentTimeMillis()),"/unitedapp/team","PUT",formatRequestForLogs(teamRequest));
         TeamEntity  teamEntity = new TeamEntity(teamRequest.getPlayerId(), teamRequest.getPlayerName(), teamRequest.getPlayerPosition());
         return teamServiceImpl.updatePlayer(teamEntity);
     }
@@ -57,13 +57,13 @@ public class TeamController {
     @GetMapping("/team/{playerId}")
     public TeamResponse getPlayer(@PathVariable(value="playerId") Integer playerId)
     {
-        dbLoggingService.logToDatabase(requestDate,"/team{"+playerId+"}","GET","");
+        dbLoggingService.logToDatabase(new Date(System.currentTimeMillis()),"/unitedapp/team{"+playerId+"}","GET","");
         return teamServiceImpl.getPlayer(playerId);
     }
 
     @RequestMapping(value = "/team/league", produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.GET)
     public String getLeagueTable() {
-        dbLoggingService.logToDatabase(requestDate,"/team/league","GET","");
+        dbLoggingService.logToDatabase(new Date(System.currentTimeMillis()),"/unitedapp/team/league","GET","");
         ResponseEntity<String> responseEntity =  teamServiceImpl.getLeagueTable();
         return Objects.requireNonNull(responseEntity.getBody());
     }
@@ -71,7 +71,7 @@ public class TeamController {
     @DeleteMapping(value = "/team/{playerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public HttpStatus deletePlayer(@PathVariable(value="playerId") Integer playerId) {
-        dbLoggingService.logToDatabase(requestDate,"/team{"+playerId+"}","DELETE","");
+        dbLoggingService.logToDatabase(new Date(System.currentTimeMillis()),"/unitedapp/team{"+playerId+"}","DELETE","");
         return teamServiceImpl.deleteByPlayerId(playerId);
     }
 
---
+++ src/main/java/com/football/unitedapp/util/KafkaConsumer.java
@@ -0,0 +1,47 @@
+package com.football.unitedapp.util;
+
+import com.football.unitedapp.dblogger.DBLoggingServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class KafkaConsumer {
+
+    SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+
+    @Autowired
+    private DBLoggingServiceImpl dbLoggingService;
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "footie23", groupId = "jacek-japila-pl")
+    public void consume(String message){
+
+        logger.info(String.format("$$$ -> Consumed Message -> %s",message));
+        dbLoggingService.logToDatabase(new Date(System.currentTimeMillis()),getEndPoint(message), "GET","");
+
+    }
+
+    private String getEndPoint(String message) {
+        String parsedMessage = message.substring(message.lastIndexOf('.') + 1).trim();
+
+        switch (parsedMessage) {
+            case "getForm())":
+                return "footiestats/api/form";
+            case "getNextFixtures())":
+                return "footiestats/api/next";
+            case "getLeagueTable())":
+                return "footiestats/api/league";
+            default:
+                return "kafka Error no match found";
+        }
+
+    }
+}

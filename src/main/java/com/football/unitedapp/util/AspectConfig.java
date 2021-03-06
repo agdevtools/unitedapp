c2da77437e0a04011f8e73cede9bf98dc02eba8a
Armand Gaillard <armandgaillard@armands-air.home>
2021-03-06 14:57:52 +0000

    added new aspect for request proxy

--- src/main/java/com/football/unitedapp/util/AspectConfig.java
+++ src/main/java/com/football/unitedapp/util/AspectConfig.java
@@ -1,15 +1,25 @@
 package com.football.unitedapp.util;
 
+import com.football.unitedapp.team.TeamRequest;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.CodeSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.logging.Logger;
 
 @Configuration
@@ -62,6 +72,7 @@ public class AspectConfig {
                     .increment();
 
         }
+    }
 
         @Aspect
         @Component
@@ -76,6 +87,55 @@ public class AspectConfig {
 
             }
         }
-    }
+
+//    @Component
+//    @Aspect
+//    public class PostRequestLogger {
+//
+//        @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+//        public void postAction() {
+//        }
+//
+//        @After("postAction()")
+//        public void logAction(JoinPoint joinPoint) {
+//            logger.info("******** Starting Request Proxy************");
+//
+//            String payload = getPayload(joinPoint);
+//            logger.info("Payload =  " + payload);
+//
+//            final String uri = "https://unitedappapi.herokuapp.com/team";
+//
+//            RestTemplate restTemplate = new RestTemplate();
+//
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+//
+//            String[] parts = payload.split("\\{");
+//            String part1 = parts[0]; // 004-
+//            String part2 = parts[1]; // 034556
+//            String part3 = "{" + part2;
+//
+//            TeamRequest teamRequest1 = new Gson().fromJson(part3, TeamRequest.class);
+//            HttpEntity<TeamRequest> entity = new HttpEntity<>(teamRequest1, headers);
+//
+//
+//            restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
+//
+//            logger.info("*******  Finishing REST CALL **********");
+//        }
+//
+//        private String getPayload(JoinPoint joinPoint) {
+//            CodeSignature signature = (CodeSignature) joinPoint.getSignature();
+//            StringBuilder builder = new StringBuilder();
+//            for (int i = 0; i < joinPoint.getArgs().length; i++) {
+//                String parameterName = signature.getParameterNames()[i];
+//                builder.append(parameterName);
+//                builder.append(": ");
+//                builder.append(joinPoint.getArgs()[i].toString());
+//            }
+//            return builder.toString();
+//        }
+//
+//    }
 
 }

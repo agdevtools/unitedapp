1f8b9782a3d36e55b2201addcb79a3eedbd0c089
Armand Gaillard <armandgaillard@Armands-MacBook-Air.local>
2021-08-03 23:00:12 +0100

    docker tets container database for tests only

--- src/test/java/com/football/unitedapp/integration/UnitedAppIntegrationTests.java
+++ src/test/java/com/football/unitedapp/integration/UnitedAppIntegrationTests.java
@@ -7,6 +7,7 @@ import com.football.unitedapp.team.TeamController;
 import com.football.unitedapp.team.TeamRequest;
 import com.football.unitedapp.team.TeamResponse;
 import org.aspectj.lang.annotation.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
@@ -19,6 +20,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,37 +40,56 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 @ContextConfiguration
 public class UnitedAppIntegrationTests {
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+//    @Autowired
+//    private WebApplicationContext webApplicationContext;
+//
+//    @Autowired
+//    private TeamRepository teamRepository;
+//
+//    private MockMvc mockMvc;
+//
+//    @BeforeEach
+//    public void initMocks() {
+//        MockitoAnnotations.initMocks(this);
+//        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+//        prepareRepoForTest();
+//    }
+//
+//    private void prepareRepoForTest() {
+//        TeamRequest teamRequest = new TeamRequest();
+//        teamRequest.setPlayerId(568);
+//        teamRequest.setPlayerName("EmbedTest-req");
+//        TeamEntity teamEntity = new TeamEntity(567,"EmbeddedTest","Test Pos");
+//        teamRepository.save(teamEntity);
+//    }
+//
+//    @Test
+//    public void testEmbeddedDb() throws Exception {
+//
+//       mockMvc.perform(get("/team/567")
+//                .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//               .andExpect(jsonPath("$.team[0].playerName", is("EmbeddedTest")));
+//
+//    }
 
-    @Autowired
-    private TeamRepository teamRepository;
-
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    public void initMocks() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        prepareRepoForTest();
-    }
-
-    private void prepareRepoForTest() {
-        TeamRequest teamRequest = new TeamRequest();
-        teamRequest.setPlayerId(568);
-        teamRequest.setPlayerName("EmbedTest-req");
-        TeamEntity teamEntity = new TeamEntity(567,"EmbeddedTest","Test Pos");
-        teamRepository.save(teamEntity);
-    }
+    @Rule
+    public PostgreSQLContainer postgresContainer = new PostgreSQLContainer();
 
     @Test
-    public void testEmbeddedDb() throws Exception {
+    public void whenSelectQueryExecuted_thenResulstsReturned()
+            throws Exception {
+        String jdbcUrl = postgresContainer.getJdbcUrl();
+        String username = postgresContainer.getUsername();
+        String password = postgresContainer.getPassword();
+        Connection conn = DriverManager
+                .getConnection(jdbcUrl, username, password);
+        ResultSet resultSet =
+                conn.createStatement().executeQuery("SELECT 1");
+        resultSet.next();
+        int result = resultSet.getInt(1);
 
-       mockMvc.perform(get("/team/567")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-               .andExpect(jsonPath("$.team[0].playerName", is("EmbeddedTest")));
-
+        assertEquals(1, result);
     }
 
 }
